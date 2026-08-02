e92568a150f8a895fc0293b0fe4b199690083c93
Nitin Kumar Singh <101050776+NitinSinghRECB@users.noreply.github.com>
2026-08-02 12:25:02 +0530

    Time: 19 ms (14.03%), Space: 19.7 MB (20.08%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& piles , int i , int j, vector<vector<int>>&dp ){
+    int solve(vector<int>&piles , int i , int j , vector<vector<int>>&dp){
         if(i>j) return 0;
 
         if(dp[i][j]!=-1) return dp[i][j];
-
-        int leftPick = piles[i]-solve(piles , i+1 , j,dp);
-        int rightPick = piles[j]-solve(piles , i , j-1,dp);
-
-        int ans  = max(leftPick , rightPick);
-        dp[i][j] = ans;
-        return dp[i][j];
+        int left = piles[i] - solve(piles , i+1 , j , dp);
+        int right = piles[j] - solve(piles , i , j-1 , dp);
+        return dp[i][j] = max(left , right);
+        
     }
     bool stoneGame(vector<int>& piles) {
-        //return true;
-        // are bhai ye kya ho gaya 
-
-        //ok ok isko bhi solve karte hai
         int n = piles.size();
-        vector<vector<int>>dp(n+1 , vector<int>(n+1 , -1)); 
-        int ans = solve(piles , 0 , piles.size()-1 , dp);
+        vector<vector<int>>dp(n+1 , vector<int>(n+1,-1));
+        int ans = solve(piles ,0,piles.size()-1,dp);
         return ans>0;
     }
 };
